1a896343392e82cee39eade6ce78221f83c946ac
Evan Bay <neopythe@protonmail.ch>
2022-06-09 17:13:05 -0600

    style navigation logo

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
