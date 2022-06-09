9274f3ec16463aeb165993f2698c164275d12a24
Evan Bay <neopythe@protonmail.ch>
2022-06-09 16:16:35 -0600

    import and render navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center text">Hello Vue</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from '@/components/MainNav.vue'
 export default {
   name: 'App',
+  components: {
+    MainNav,
+  },
 }
 </script>
