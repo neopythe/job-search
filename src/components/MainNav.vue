220666a01c08558ac3b26080602ecb36fad1ac60
Evan Bay <neopythe@protonmail.ch>
2022-06-10 13:05:39 -0600

    add click listener for login functionality on main navigation

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -23,7 +23,7 @@
         </nav>
         <div class="flex items-center h-full ml-auto">
           <ProfileImage v-if="isLoggedIn" data-test="profile-image" />
-          <ActionButton v-else data-test="login-button" />
+          <ActionButton v-else data-test="login-button" @click="loginUser" />
         </div>
       </div>
     </div>
@@ -54,6 +54,11 @@ export default {
       ],
       isLoggedIn: false,
     }
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true
+    },
   },
 }
 </script>
