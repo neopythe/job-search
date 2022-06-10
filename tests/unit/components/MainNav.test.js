2e0f86bda8bb66e7a57f7f2b59b766f34299e4c7
Evan Bay <neopythe@protonmail.ch>
2022-06-10 10:02:43 -0600

    test v-for directive

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,20 @@ describe('MainNav', () => {
     const wrapper = mount(MainNav)
     expect(wrapper.text()).toMatch('Gaggle Careers')
   })
+
+  it('displays menu items for navigation', () => {
+    const wrapper = mount(MainNav)
+    const navigationMenuItems = wrapper.findAll(
+      '[data-test="main-nav-list-item"]'
+    )
+    const navigationMenuTexts = navigationMenuItems.map(item => item.text())
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Gaggle',
+      'How we hire',
+      'Students',
+      'Jobs',
+    ])
+  })
 })
