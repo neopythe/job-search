38219582c2401f3f522bc969d8ad1dd033d7fe04
Evan Bay <neopythe@protonmail.ch>
2022-06-10 17:43:58 -0600

    add tests for subnav component

---
+++ tests/unit/components/SubNav.test.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+
+import SubNav from '@/components/SubNav.vue'
+
+describe('SubNav', () => {
+  describe('when the user is on the jobs page', () => {
+    it('displays job count', () => {
+      const wrapper = mount(SubNav, {
+        data() {
+          return {
+            onJobResultsPage: true,
+          }
+        },
+      })
+      const jobCount = wrapper.find('[data-test="job-count"]')
+      expect(jobCount.exists()).toBe(true)
+    })
+  })
+
+  describe('when the user is not on the jobs page', () => {
+    it('does not display job count', () => {
+      const wrapper = mount(SubNav, {
+        data() {
+          return {
+            onJobResultsPage: false,
+          }
+        },
+      })
+      const jobCount = wrapper.find('[data-test="job-count"]')
+      expect(jobCount.exists()).toBe(false)
+    })
+  })
+})
