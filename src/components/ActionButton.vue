7c6f45b872f15381519c534eb85ac5d085137fea
Evan Bay <neopythe@protonmail.ch>
2022-06-10 13:58:39 -0600

    add v-bind to action button class attribute

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,22 @@
 <template>
-  <button
-    class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign in
+  <button :class="{ primary: false }">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: 'ActionButton',
+  props: ['text'],
 }
 </script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 font-medium rounded;
+}
+
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
