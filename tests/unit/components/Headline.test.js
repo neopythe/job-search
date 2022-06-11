40845a0404511f89c19f654f7630024935ac7f6c
Evan Bay <neopythe@protonmail.ch>
2022-06-11 14:32:22 -0600

    run additional tests for headline component

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -1,3 +1,4 @@
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 
 import Headline from '@/components/Headline.vue'
@@ -8,6 +9,23 @@ describe('Headline', () => {
     const wrapper = mount(Headline)
     const actionPhrase = wrapper.find('[data-test="action-phrase"]')
     expect(actionPhrase.text()).toBe('Build for everyone')
+    jest.useRealTimers()
+  })
+
+  it('changes action verb at a consistent interval', () => {
+    jest.useFakeTimers('legacy')
+    mount(Headline)
+    expect(setInterval).toHaveBeenCalled()
+    jest.useRealTimers()
+  })
+
+  it('swaps action verb after first interval', async () => {
+    jest.useFakeTimers('legacy')
+    const wrapper = mount(Headline)
+    jest.runOnlyPendingTimers()
+    await nextTick() // update DOM
+    const actionPhrase = wrapper.find('[data-test="action-phrase"]')
+    expect(actionPhrase.text()).toBe('Create for everyone')
     jest.useRealTimers()
   })
 })
