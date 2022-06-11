efc4ccdfd44ec727ae13312c7cd2ff04e5200f74
Evan Bay <neopythe@protonmail.ch>
2022-06-11 10:41:36 -0600

    create headline component

---
+++ src/components/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Gaggle</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Headline',
+  data() {
+    return {
+      sample: 'Hello',
+    }
+  },
+  created() {
+    console.log(`Hey, I've been created. My data exists! Look: ${this.sample}`)
+  },
+}
+</script>
