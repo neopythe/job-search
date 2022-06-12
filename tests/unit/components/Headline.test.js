d857402eca43cb92ccf813daee80f42d1235c5a9
Evan Bay <neopythe@protonmail.ch>
2022-06-11 19:09:58 -0600

    add beforeeach and aftereach functions to headline tests

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -4,36 +4,36 @@ import { mount } from '@vue/test-utils'
 import Headline from '@/components/Headline.vue'
 
 describe('Headline', () => {
-  it('displays introductory action verb', () => {
+  beforeEach(() => {
     jest.useFakeTimers('legacy')
+  })
+
+  afterEach(() => {
+    jest.useRealTimers()
+  })
+
+  it('displays introductory action verb', () => {
     const wrapper = mount(Headline)
     const actionPhrase = wrapper.find('[data-test="action-phrase"]')
     expect(actionPhrase.text()).toBe('Build for everyone')
-    jest.useRealTimers()
   })
 
   it('changes action verb at a consistent interval', () => {
-    jest.useFakeTimers('legacy')
     mount(Headline)
     expect(setInterval).toHaveBeenCalled()
-    jest.useRealTimers()
   })
 
   it('swaps action verb after first interval', async () => {
-    jest.useFakeTimers('legacy')
     const wrapper = mount(Headline)
     jest.runOnlyPendingTimers()
     await nextTick() // update DOM
     const actionPhrase = wrapper.find('[data-test="action-phrase"]')
     expect(actionPhrase.text()).toBe('Create for everyone')
-    jest.useRealTimers()
   })
 
   it('removes interval when component disappears', () => {
-    jest.useFakeTimers('legacy')
     const wrapper = mount(Headline)
     wrapper.unmount()
     expect(clearInterval).toHaveBeenCalled()
-    jest.useRealTimers()
   })
 })
