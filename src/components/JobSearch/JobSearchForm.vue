164bcdbaa6b95cdcf2824a16ced5836767d25300
Evan Bay <neopythe@protonmail.ch>
2022-06-15 01:00:57 -0600

    use textinput components in job search form

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,12 +6,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-8">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg focus:outline-none"
-        />
+        <TextInput placeholder="Software engineer" />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2 font-light"
@@ -19,12 +14,7 @@
       >
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-3 -top-8">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-full text-lg focus:outline-none"
-        />
+        <TextInput placeholder="Los Angeles" />
       </div>
     </div>
     <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
@@ -33,10 +23,12 @@
 
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
+import TextInput from '@/components/Shared/TextInput.vue'
 export default {
   name: 'JobSearchForm',
   components: {
     ActionButton,
+    TextInput,
   },
   data() {
     return {
