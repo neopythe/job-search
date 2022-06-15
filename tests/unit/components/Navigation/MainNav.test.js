7617711f40d10af92003c38b61f97aab190a1d18
Evan Bay <neopythe@protonmail.ch>
2022-06-15 15:39:38 -0600

    refactor test suite with factory function

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -3,23 +3,21 @@ import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import MainNav from '@/components/Navigation/MainNav.vue'
 
 describe('MainNav', () => {
-  let wrapper
-
-  beforeEach(() => {
-    wrapper = shallowMount(MainNav, {
-      global: {
-        stubs: {
-          'router-link': RouterLinkStub,
-        },
+  const createConfig = () => ({
+    global: {
+      stubs: {
+        'router-link': RouterLinkStub,
       },
-    })
+    },
   })
 
   it('displays company name', () => {
+    const wrapper = shallowMount(MainNav, createConfig())
     expect(wrapper.text()).toMatch('Gaggle Careers')
   })
 
   it('displays menu items for navigation', () => {
+    const wrapper = shallowMount(MainNav, createConfig())
     const navigationMenuItems = wrapper.findAll(
       '[data-test="main-nav-list-item"]'
     )
@@ -36,6 +34,7 @@ describe('MainNav', () => {
 
   describe('when user is logged out', () => {
     it('prompts user to sign in', () => {
+      const wrapper = shallowMount(MainNav, createConfig())
       const loginButton = wrapper.find('[data-test="login-button"]')
       expect(loginButton.exists()).toBe(true)
     })
@@ -43,6 +42,7 @@ describe('MainNav', () => {
 
   describe('when user logs in', () => {
     it('displays user profile picture', async () => {
+      const wrapper = shallowMount(MainNav, createConfig())
       let profileImage = wrapper.find('[data-test="profile-image"]')
       expect(profileImage.exists()).toBe(false)
 
@@ -54,6 +54,7 @@ describe('MainNav', () => {
     })
 
     it('displays subnavigation menu with additional information', async () => {
+      const wrapper = shallowMount(MainNav, createConfig())
       let subNav = wrapper.find('[data-test="subnav"]')
       expect(subNav.exists()).toBe(false)
 
