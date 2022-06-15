fca21ab83cb10bbf51becf30068b5ddd9c8b47b1
Evan Bay <neopythe@protonmail.ch>
2022-06-15 00:36:18 -0600

    isolate shared components

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -37,7 +37,7 @@
 </template>
 
 <script>
-import ActionButton from '@/components/ActionButton.vue'
+import ActionButton from '@/components/Shared/ActionButton.vue'
 import ProfileImage from '@/components/Navigation/ProfileImage.vue'
 import SubNav from '@/components/Navigation/SubNav.vue'
 
