ae94f9d8d483df2416fb4beeacfe6319f58717eb
Evan Bay <neopythe@protonmail.ch>
2022-06-15 21:12:51 -0600

    update job listing to render job data

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,18 +1,28 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-1 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
         <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmented Reality
+          {{ job.title }}
         </h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Gaggle</span>
+            <span>{{ job.organization }}</span>
           </div>
-          <div>San Francisco, CA, USA</div>
+          <div>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div class="px-8 py-4">
@@ -20,17 +30,17 @@
           <h3 class="mt-1 mb-2">Qualifications</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent practice experience.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Experience analyzing data through querying database (e.g. SQL),
-                using spreadsheet software, and creating statistical models.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-3 text-center">
-          <router-link to="/jobs/results/1" class="text-sm text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-sm text-brand-blue-1"
             >Expand</router-link
           >
         </div>
