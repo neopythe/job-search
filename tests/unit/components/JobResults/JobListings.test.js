678d227de1d0ea9da50f3e84ee82493e47183889
Evan Bay <neopythe@protonmail.ch>
2022-06-16 00:36:22 -0600

    create first tests for joblistings component

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,2 +1,20 @@
+import { shallowMount } from '@vue/test-utils'
 import axios from 'axios'
 jest.mock('axios')
+
+import JobListings from '@/components/JobResults/JobListings.vue'
+
+describe('JobListings', () => {
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] })
+    shallowMount(JobListings)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
+  })
+
+  it('creates a job listing for each received job', () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    const wrapper = shallowMount(JobListings)
+    const jobListings = wrapper.findAll('[data-test="job-listing"]')
+    expect(jobListings).toHaveLength(15)
+  })
+})
