1538643983cdc78af8b8e9c837728b9dd8007e24
Evan Bay <neopythe@protonmail.ch>
2022-06-16 14:08:34 -0600

    refactor joblistings component tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -5,39 +5,33 @@ jest.mock('axios')
 import JobListings from '@/components/JobResults/JobListings.vue'
 
 describe('JobListings', () => {
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: '5',
+      ...queryParams,
+    },
+  })
+
+  const createConfig = $route => ({
+    global: {
+      mocks: {
+        $route,
+      },
+    },
+  })
+
   it('fetches jobs', () => {
     axios.get.mockResolvedValue({ data: [] })
-    const $route = {
-      query: {
-        page: '5',
-      },
-    }
-
-    shallowMount(JobListings, {
-      global: {
-        mocks: {
-          $route,
-        },
-      },
-    })
+    const $route = createRoute()
+    shallowMount(JobListings, createConfig($route))
     expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
   })
 
   it('creates a job listing for a maximum of 10 jobs', async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) })
-    const $route = {
-      query: {
-        page: '1',
-      },
-    }
-
-    const wrapper = shallowMount(JobListings, {
-      global: {
-        mocks: {
-          $route,
-        },
-      },
-    })
+    const queryParams = { page: '1' }
+    const $route = createRoute(queryParams)
+    const wrapper = shallowMount(JobListings, createConfig($route))
     await flushPromises()
     const jobListings = wrapper.findAll('[data-test="job-listing"]')
     expect(jobListings).toHaveLength(10)
