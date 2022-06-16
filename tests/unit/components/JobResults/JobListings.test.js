6c514c1418159385d63b582106b7e8745f14da32
Evan Bay <neopythe@protonmail.ch>
2022-06-16 15:57:59 -0600

    clean up test pollution

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -5,6 +5,14 @@ jest.mock('axios')
 import JobListings from '@/components/JobResults/JobListings.vue'
 
 describe('JobListings', () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+  })
+
+  afterEach(() => {
+    axios.get.mockReset()
+  })
+
   const createRoute = (queryParams = {}) => ({
     query: {
       page: '5',
@@ -24,7 +32,6 @@ describe('JobListings', () => {
   })
 
   it('fetches jobs', () => {
-    axios.get.mockResolvedValue({ data: [] })
     const $route = createRoute()
     shallowMount(JobListings, createConfig($route))
     expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
