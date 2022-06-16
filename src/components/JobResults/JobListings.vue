753a5f3be959f2f1c43219dfc29935794bcf1118
Evan Bay <neopythe@protonmail.ch>
2022-06-15 19:40:13 -0600

    render multiple job listings in joblistings.vue

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,11 +1,20 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
-    <ol></ol>
+    <ol>
+      <JobListing />
+      <JobListing />
+      <JobListing />
+    </ol>
   </main>
 </template>
 
 <script>
+import JobListing from '@/components/JobResults/JobListing.vue'
+
 export default {
   name: 'JobListings',
+  components: {
+    JobListing,
+  },
 }
 </script>
