317ef35ab384fa4863eec5531f193bde618294cd
Evan Bay <neopythe@protonmail.ch>
2022-06-15 19:22:51 -0600

    add job title and location to a job listing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,5 +1,26 @@
 <template>
-  <main class="flex-auto p-8 bg-brand-gray-2">Job Listings Component</main>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="block mx-auto bg-white border border-solid border-brand-gray-1 rounded hover:shadow-gray"
+        >
+          <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
+            <h2 class="mb-2 text-2xl">
+              Technical Program Manager, Perception, Augmented Reality
+            </h2>
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Gaggle</span>
+              </div>
+              <div>San Francisco, CA, USA</div>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
