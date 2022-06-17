ffd0d6156472f750787efed525a1188ebdd945c3
Evan Bay <neopythe@protonmail.ch>
2022-06-17 15:32:52 -0600

    map state from vuex store to mainnav component

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -41,6 +41,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import ProfileImage from '@/components/Navigation/ProfileImage.vue'
 import SubNav from '@/components/Navigation/SubNav.vue'
@@ -72,9 +74,7 @@ export default {
         'h-32': this.isLoggedIn,
       }
     },
-    isLoggedIn() {
-      return this.$store.state.isLoggedIn
-    },
+    ...mapState(['isLoggedIn']),
   },
   methods: {
     loginUser() {
