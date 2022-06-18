67176b0be67683280ff1e713699be36d574dc1cc
Evan Bay <neopythe@protonmail.ch>
2022-06-18 03:40:06 -0600

    test fetch_jobs action

--- tests/unit/store/index.test.js
+++ tests/unit/store/index.test.js
@@ -1,4 +1,6 @@
-import { state, mutations } from '@/store'
+import { state, mutations, actions } from '@/store'
+import getJobs from '@/api/getJobs'
+jest.mock('@/api/getJobs')
 
 describe('state', () => {
   it('keeps track of whether user is logged in', () => {
@@ -26,6 +28,39 @@ describe('mutations', () => {
       const state = { jobs: [] }
       mutations.RECEIVE_JOBS(state, ['Job 1', 'Job 2'])
       expect(state).toEqual({ jobs: ['Job 1', 'Job 2'] })
+    })
+  })
+})
+
+describe('actions', () => {
+  describe('FETCH_JOBS', () => {
+    beforeEach(() => {
+      getJobs.mockResolvedValue([
+        {
+          id: 1,
+          title: 'Software Developer',
+        },
+      ])
+    })
+
+    it('makes a request to fetch jobs', async () => {
+      const context = {
+        commit: jest.fn(),
+      }
+      await actions.FETCH_JOBS(context)
+      expect(getJobs).toHaveBeenCalled()
+    })
+
+    it('sends message to save received jobs in store', async () => {
+      const commit = jest.fn()
+      const context = { commit }
+      await actions.FETCH_JOBS(context)
+      expect(commit).toHaveBeenCalledWith('RECEIVE_JOBS', [
+        {
+          id: 1,
+          title: 'Software Developer',
+        },
+      ])
     })
   })
 })
