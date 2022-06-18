a33d3ff77eaaf20655731c094eeccf1ae467403e
Evan Bay <neopythe@protonmail.ch>
2022-06-18 16:12:39 -0600

    create templates for named slots on teamsview

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,14 @@
 <template>
-  <HeaderContainer />
+  <HeaderContainer>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template v-slot:subtitle>
+      <h2 class="w-full my-4 text-base font-light">
+        It's awesome working here. Why don't you come join us?
+      </h2>
+    </template>
+  </HeaderContainer>
 </template>
 
 <script>
