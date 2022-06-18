f627e28c2ad28eb80f3c15ea1359626b3fcd4b96
Evan Bay <neopythe@protonmail.ch>
2022-06-18 14:50:41 -0600

    extract organizations dropdown to new component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -13,30 +13,7 @@
       <Accordion header="Job types">
         <p>Placeholder for now</p>
       </Accordion>
-      <Accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="w-1/2 h-8">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </Accordion>
+      <JobFiltersSidebarOrganizations />
     </section>
   </div>
 </template>
@@ -44,12 +21,14 @@
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import Accordion from '@/components/Shared/Accordion.vue'
+import JobFiltersSidebarOrganizations from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue'
 
 export default {
   name: 'JobFiltersSidebar',
   components: {
     ActionButton,
     Accordion,
+    JobFiltersSidebarOrganizations,
   },
 }
 </script>
