7c81bd1ea02fe8f562c7205c52d259cbb7d779d4
Evan Bay <neopythe@protonmail.ch>
2022-06-18 18:23:28 -0600

    pass multiple props from spotlight component

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -22,12 +22,12 @@
           :to="{ name: 'JobResults' }"
           class="flex flex-col mx-5 border rounded-lg w-72 bg-brand-gray-2 h-96 overflow-hidden"
         >
-          <img :src="slotProps.spotlight.img" class="object-contain" />
+          <img :src="slotProps.img" class="object-contain" />
           <div class="h-48 px-6 py-4 mt-">
             <h3 class="text-lg font-semibold">
-              {{ slotProps.spotlight.title }}
+              {{ slotProps.title }}
             </h3>
-            <p class="mt-3 text-sm">{{ slotProps.spotlight.description }}</p>
+            <p class="mt-3 text-sm">{{ slotProps.description }}</p>
           </div>
           <router-link
             :to="{ name: 'JobResults' }"
