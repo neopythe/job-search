4edb633a41823128da10439fc705774c00afd6d5
Evan Bay <neopythe@protonmail.ch>
2022-06-18 18:08:01 -0600

    render spotlight components

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-10 pb-20">
+  <main class="flex flex-col h-screen">
+    <section class="flex flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1"></div>
         <div class="col-start-2 col-span-5">
@@ -16,18 +16,41 @@
         <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
+    <Spotlight class="flex flex-row justify-center pb-16">
+      <template #default="slotProps">
+        <router-link
+          :to="{ name: 'JobResults' }"
+          class="flex flex-col mx-5 border rounded-lg w-72 bg-brand-gray-2 h-96 overflow-hidden"
+        >
+          <img :src="slotProps.spotlight.img" class="object-contain" />
+          <div class="h-48 px-6 py-4 mt-">
+            <h3 class="text-lg font-semibold">
+              {{ slotProps.spotlight.title }}
+            </h3>
+            <p class="mt-3 text-sm">{{ slotProps.spotlight.description }}</p>
+          </div>
+          <router-link
+            :to="{ name: 'JobResults' }"
+            class="px-6 pb-4 text-sm text-brand-blue-1 font-semibold"
+            >See jobs</router-link
+          >
+        </router-link>
+      </template>
+    </Spotlight>
   </main>
 </template>
 
 <script>
 import Headline from '@/components/JobSearch/Headline.vue'
 import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue'
+import Spotlight from '@/components/JobSearch/Spotlight.vue'
 
 export default {
   name: 'Hero',
   components: {
     Headline,
     JobSearchForm,
+    Spotlight,
   },
 }
 </script>
