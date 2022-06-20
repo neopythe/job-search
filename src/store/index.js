6b99382e09a730b2e637c40b86c286772690617a
Evan Bay <neopythe@protonmail.ch>
2022-06-20 13:48:42 -0600

    reorder vuex store constants

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import { createStore } from 'vuex'
 
 import getJobs from '@/api/getJobs'
 
+export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
+
 export const LOGIN_USER = 'LOGIN_USER'
 export const RECEIVE_JOBS = 'RECEIVE_JOBS'
-export const FETCH_JOBS = 'FETCH_JOBS'
 
-export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
+export const FETCH_JOBS = 'FETCH_JOBS'
 
 export const state = () => {
   return {
