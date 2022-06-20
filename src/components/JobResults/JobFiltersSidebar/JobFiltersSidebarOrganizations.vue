6da2c905805836040b0a93766d484f727d80dd1c
Evan Bay <neopythe@protonmail.ch>
2022-06-20 17:46:22 -0600

    commit mutation to receive organizations from jobfilterssidebarorganizations component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -27,9 +27,9 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 
-import { UNIQUE_ORGANIZATIONS } from '@/store'
+import { ADD_SELECTED_ORGANIZATIONS, UNIQUE_ORGANIZATIONS } from '@/store'
 
 import Accordion from '@/components/Shared/Accordion.vue'
 
@@ -47,7 +47,10 @@ export default {
     ...mapGetters([UNIQUE_ORGANIZATIONS]),
   },
   methods: {
-    selectOrganization() {},
+    ...mapMutations([ADD_SELECTED_ORGANIZATIONS]),
+    selectOrganization() {
+      this.ADD_SELECTED_ORGANIZATIONS(this.selectedOrganizations)
+    },
   },
 }
 </script>
