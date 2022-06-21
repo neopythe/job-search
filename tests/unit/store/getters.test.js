0d29d797411fbd05042b2c9596e5d364e1604dd4
Evan Bay <neopythe@protonmail.ch>
2022-06-21 16:19:52 -0600

    test getters with arguments

--- tests/unit/store/getters.test.js
+++ tests/unit/store/getters.test.js
@@ -75,6 +75,50 @@ describe('getters', () => {
     })
   })
 
+  describe('INCLUDE_JOB_BY_JOB_TYPE', () => {
+    describe('when the user has not selected any job types', () => {
+      it('includes job', () => {
+        const state = {
+          selectedJobTypes: [],
+        }
+        const job = { jobType: 'Full-time' }
+        const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job)
+        expect(includeJob).toBe(true)
+      })
+
+      it('identifies if job is associated with given job types', () => {
+        const state = {
+          selectedJobTypes: ['Part-time', 'Part-time'],
+        }
+        const job = { jobType: 'Part-time' }
+        const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job)
+        expect(includeJob).toBe(true)
+      })
+    })
+  })
+
+  describe('INCLUDE_JOB_BY_ORGANIZATION', () => {
+    describe('when the user has not selected any organizations', () => {
+      it('includes job', () => {
+        const state = {
+          selectedOrganizations: [],
+        }
+        const job = { organization: 'Gaggle' }
+        const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job)
+        expect(includeJob).toBe(true)
+      })
+
+      it('identifies if job is associated with given organizations', () => {
+        const state = {
+          selectedOrganizations: ['Gaggle', 'MikeRoweSoft'],
+        }
+        const job = { organization: 'Gaggle' }
+        const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job)
+        expect(includeJob).toBe(true)
+      })
+    })
+  })
+
   describe('UNIQUE_JOB_TYPES', () => {
     it('finds unique job types from list of jobs', () => {
       const state = {
