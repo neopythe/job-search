3d1cf50684e65e3a627877047593593d1f3f0995
Evan Bay <neopythe@protonmail.ch>
2022-06-20 22:54:03 -0600

    update header text colour

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header :class="['w-full', 'text-sm', headerHeightClass]">
+  <header :class="['w-full', 'text-sm', 'text-gray-600', headerHeightClass]">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
