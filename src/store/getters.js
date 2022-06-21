40601b8a8b20953541adbb48e3542c6527789d7b
Evan Bay <neopythe@protonmail.ch>
2022-06-21 17:10:11 -0600

    delete extraneous getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,5 @@
 import {
   FILTERED_JOBS,
-  FILTERED_JOBS_BY_JOB_TYPES,
-  FILTERED_JOBS_BY_ORGANIZATIONS,
   INCLUDE_JOB_BY_JOB_TYPE,
   INCLUDE_JOB_BY_ORGANIZATION,
   UNIQUE_JOB_TYPES,
@@ -13,22 +11,6 @@ const getters = {
     return state.jobs
       .filter(job => getters.INCLUDE_JOB_BY_JOB_TYPE(job))
       .filter(job => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
-  },
-  [FILTERED_JOBS_BY_JOB_TYPES](state) {
-    if (state.selectedJobTypes.length === 0) {
-      return state.jobs
-    }
-    return state.jobs.filter(job =>
-      state.selectedJobTypes.includes(job.jobType)
-    )
-  },
-  [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
-    if (state.selectedOrganizations.length === 0) {
-      return state.jobs
-    }
-    return state.jobs.filter(job =>
-      state.selectedOrganizations.includes(job.organization)
-    )
   },
   [INCLUDE_JOB_BY_JOB_TYPE]: state => job => {
     if (state.selectedJobTypes.length === 0) return true
