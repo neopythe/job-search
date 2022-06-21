79a3a52df2b8849fe8c3f0b1f3614934b77a9733
Evan Bay <neopythe@protonmail.ch>
2022-06-21 12:37:29 -0600

    add mutation for selecting job types

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,10 +1,14 @@
 import {
+  ADD_SELECTED_JOB_TYPES,
   ADD_SELECTED_ORGANIZATIONS,
   LOGIN_USER,
   RECEIVE_JOBS,
 } from '@/store/constants'
 
 const mutations = {
+  [ADD_SELECTED_JOB_TYPES](state, jobTypes) {
+    state.selectedJobTypes = jobTypes
+  },
   [ADD_SELECTED_ORGANIZATIONS](state, organizations) {
     state.selectedOrganizations = organizations
   },
--- tests/unit/store/mutations.test.js
+++ tests/unit/store/mutations.test.js
@@ -1,6 +1,26 @@
 import mutations from '@/store/mutations'
 
 describe('mutations', () => {
+  describe('ADD_SELECTED_JOB_TYPES', () => {
+    it('updates job types that the user has chosen to filter jobs by', () => {
+      const state = { selectedJobTypes: [] }
+      mutations.ADD_SELECTED_JOB_TYPES(state, ['Full-time', 'Part-time'])
+      expect(state).toEqual({
+        selectedJobTypes: ['Full-time', 'Part-time'],
+      })
+    })
+  })
+
+  describe('ADD_SELECTED_ORGANIZATIONS', () => {
+    it('updates organizations that the user has chosen to filter jobs by', () => {
+      const state = { selectedOrganizations: [] }
+      mutations.ADD_SELECTED_ORGANIZATIONS(state, ['Org 1', 'Org 2'])
+      expect(state).toEqual({
+        selectedOrganizations: ['Org 1', 'Org 2'],
+      })
+    })
+  })
+
   describe('LOGIN_USER', () => {
     it('logs the user in', () => {
       const state = { isLoggedIn: false }
@@ -14,16 +34,6 @@ describe('mutations', () => {
       const state = { jobs: [] }
       mutations.RECEIVE_JOBS(state, ['Job 1', 'Job 2'])
       expect(state).toEqual({ jobs: ['Job 1', 'Job 2'] })
-    })
-  })
-
-  describe('ADD_SELECTED_ORGANIZATIONS', () => {
-    it('updates organizations that the user has chosen to filter jobs by', () => {
-      const state = { selectedOrganizations: [] }
-      mutations.ADD_SELECTED_ORGANIZATIONS(state, ['Org 1', 'Org 2'])
-      expect(state).toEqual({
-        selectedOrganizations: ['Org 1', 'Org 2'],
-      })
     })
   })
 })
