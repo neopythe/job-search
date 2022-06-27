131afe4c96a66065c0dfe15df738f224d5e0da21
Evan Bay <neopythe@protonmail.ch>
2022-06-27 14:35:02 -0600

    add types to getters file

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -6,27 +6,36 @@ import {
   UNIQUE_ORGANIZATIONS,
 } from '@/store/constants'
 
+import { GlobalState } from '@/store/types'
+import { Job } from '@/api/types'
+
 const getters = {
-  [FILTERED_JOBS](state, getters) {
+  [FILTERED_JOBS](
+    state: GlobalState,
+    getters: {
+      INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean
+      INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean
+    }
+  ) {
     return state.jobs
       .filter(job => getters.INCLUDE_JOB_BY_JOB_TYPE(job))
       .filter(job => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
   },
-  [INCLUDE_JOB_BY_JOB_TYPE]: state => job => {
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state: GlobalState) => (job: Job) => {
     if (state.selectedJobTypes.length === 0) return true
     return state.selectedJobTypes.includes(job.jobType)
   },
-  [INCLUDE_JOB_BY_ORGANIZATION]: state => job => {
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state: GlobalState) => (job: Job) => {
     if (state.selectedOrganizations.length === 0) return true
     return state.selectedOrganizations.includes(job.organization)
   },
-  [UNIQUE_JOB_TYPES](state) {
-    const uniqueJobTypes = new Set()
+  [UNIQUE_JOB_TYPES](state: GlobalState) {
+    const uniqueJobTypes = new Set<string>()
     state.jobs.forEach(job => uniqueJobTypes.add(job.jobType))
     return uniqueJobTypes
   },
-  [UNIQUE_ORGANIZATIONS](state) {
-    const uniqueOrganizations = new Set()
+  [UNIQUE_ORGANIZATIONS](state: GlobalState) {
+    const uniqueOrganizations = new Set<string>()
     state.jobs.forEach(job => uniqueOrganizations.add(job.organization))
     return uniqueOrganizations
   },
