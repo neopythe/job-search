31218cbf6859b93811140d01298f1875d9a4857d
Evan Bay <neopythe@protonmail.ch>
2022-06-28 09:32:04 -0600

    annotate views

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -11,13 +11,15 @@
   </HeaderContainer>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
 import HeaderContainer from '@/components/Shared/HeaderContainer.vue'
 
-export default {
+export default defineComponent({
   name: 'TeamsView',
   components: {
     HeaderContainer,
   },
-}
+})
 </script>
