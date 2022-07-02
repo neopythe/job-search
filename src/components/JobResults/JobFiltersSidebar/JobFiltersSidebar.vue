35be2ee2b314e951b0ebcd1d25c5aab80560ff30
Evan Bay <neopythe@protonmail.ch>
2022-07-02 06:47:08 -0600

    read search term from query params

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -21,7 +21,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+
+import { key } from '@/store'
+
+import { UPDATE_SKILLS_SEARCH_TERM } from '@/store/constants'
 
 import Accordion from '@/components/Shared/Accordion.vue'
 
@@ -40,6 +46,18 @@ export default defineComponent({
     JobFiltersSidebarOrganizations,
     JobFiltersSidebarPrompt,
     JobFiltersSidebarSkills,
+  },
+  setup() {
+    const parseSkillsSearchTerm = () => {
+      const route = useRoute()
+      const store = useStore(key)
+
+      const role = route.query.role || ''
+
+      store.commit(UPDATE_SKILLS_SEARCH_TERM, role)
+    }
+
+    onMounted(parseSkillsSearchTerm)
   },
 })
 </script>
