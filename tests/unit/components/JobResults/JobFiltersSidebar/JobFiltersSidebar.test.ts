89023c46eb545ce0d1f149b70e096ba2c66bf640
Evan Bay <neopythe@protonmail.ch>
2022-07-02 02:12:57 -0600

    break up former jobfilterssidebar component tests

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.test.ts
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.test.ts
@@ -1,61 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
 jest.mock('vuex')
 
-import {
-  useUniqueDegrees,
-  useUniqueJobTypes,
-  useUniqueOrganizations,
-} from '@/store/composables'
-jest.mock('@/store/composables')
-const useUniqueDegreesMock = useUniqueDegrees as jest.Mock
-const useUniqueJobTypesMock = useUniqueJobTypes as jest.Mock
-const useUniqueOrganizationsMock = useUniqueOrganizations as jest.Mock
-
 import JobFiltersSidebar from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue'
 
 describe('JobFiltersSidebar', () => {
-  it('allows user to filter jobs by degrees', () => {
-    useUniqueDegreesMock.mockReturnValue(['Associate', "Bachelor's"])
-    useUniqueJobTypesMock.mockReturnValue(new Set(['Full-time', 'Part-time']))
-    useUniqueOrganizationsMock.mockReturnValue(new Set(['Javazon']))
+  it('sets up panel for user to filter jobs by one or more criteria', () => {
     const wrapper = shallowMount(JobFiltersSidebar)
-    const degreesFilter = wrapper.findComponent<HTMLElement>(
-      '[data-test="degrees-filter"]'
-    )
-    const { header, mutation, uniqueValues } = degreesFilter.props()
 
-    expect(header).toBe('Degree')
-    expect(mutation).toBe('ADD_SELECTED_DEGREES')
-    expect(uniqueValues).toEqual(['Associate', "Bachelor's"])
-  })
-
-  it('allows user to filter jobs by job types', () => {
-    useUniqueDegreesMock.mockReturnValue(['Associate', "Bachelor's"])
-    useUniqueJobTypesMock.mockReturnValue(new Set(['Full-time', 'Part-time']))
-    useUniqueOrganizationsMock.mockReturnValue(new Set(['Javazon']))
-    const wrapper = shallowMount(JobFiltersSidebar)
-    const jobTypesFilter = wrapper.findComponent<HTMLElement>(
-      '[data-test="job-types-filter"]'
-    )
-    const { header, mutation, uniqueValues } = jobTypesFilter.props()
-
-    expect(header).toBe('Job types')
-    expect(mutation).toBe('ADD_SELECTED_JOB_TYPES')
-    expect(uniqueValues).toEqual(new Set(['Full-time', 'Part-time']))
-  })
-
-  it('allows user to filter jobs by organizations', () => {
-    useUniqueDegreesMock.mockReturnValue(['Associate', "Bachelor's"])
-    useUniqueJobTypesMock.mockReturnValue(new Set(['Full-time', 'Part-time']))
-    useUniqueOrganizationsMock.mockReturnValue(new Set(['Javazon']))
-    const wrapper = shallowMount(JobFiltersSidebar)
-    const organizationsFilter = wrapper.findComponent<HTMLElement>(
-      '[data-test="organizations-filter"]'
-    )
-    const { header, mutation, uniqueValues } = organizationsFilter.props()
-
-    expect(header).toBe('Organizations')
-    expect(mutation).toBe('ADD_SELECTED_ORGANIZATIONS')
-    expect(uniqueValues).toEqual(new Set(['Javazon']))
+    expect(wrapper.exists()).toBe(true)
   })
 })
