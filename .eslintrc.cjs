28ea747eadc30a1431bf0e667873621d34de12ee
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-13 11:15:03 -0700

    configure testing environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,13 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  env: {
+    "vitest-globals/env": true,
   },
   overrides: [
     {
