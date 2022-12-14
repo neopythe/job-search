67bd9d57626fe6f834dcfe94f23a08e818ce609a
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-14 14:29:38 -0700

    render HeroSection in App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <main-nav />
+  <hero-section />
 </template>
 
 <script>
+import HeroSection from "@/components/HeroSection.vue";
 import MainNav from "@/components/MainNav.vue";
 
 export default {
   name: "App",
   components: {
+    HeroSection,
     MainNav,
   },
 };
