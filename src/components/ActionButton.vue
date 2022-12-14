75167b3036bb59fe1fdf5cad85cdd6517580e564
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-14 10:29:21 -0700

    make ActionButton customizable via props

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,39 @@
 <template>
-  <button
-    class="whitespace-nowrap rounded border-0 bg-brand-blue-1 px-5 py-2.5 text-xs font-semibold text-white hover:shadow-blue"
-  >
-    Sign in
+  <button :class="type">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "primary",
+      validator(value) {
+        return ["primary", "secondary"].includes(value);
+      },
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply whitespace-nowrap rounded px-5 py-2.5 font-semibold hover:shadow-blue;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-xs text-white;
+}
+
+.secondary {
+  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
