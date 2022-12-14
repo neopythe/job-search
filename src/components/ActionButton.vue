29055136d98d987650c6f819b843900acfe9299f
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-14 06:07:56 -0700

    update styles for ActionButton

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-2.5 text-xs font-semibold text-white hover:shadow-blue"
+    class="whitespace-nowrap rounded border-0 bg-brand-blue-1 px-5 py-2.5 text-xs font-semibold text-white hover:shadow-blue"
   >
     Sign in
   </button>
