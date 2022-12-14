9a109240de3abaec7d3e897cf478cc6edee82537
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-14 06:08:15 -0700

    create ProfileImage component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imageLink"
+    class="h-8 w-8 rounded-full object-contain"
+    alt="User profile image"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://64.media.tumblr.com/32c356e67d5cdd6e3f2ab4d4cf3b6fce/tumblr_ntb2c29J7Z1uzn48xo3_640.jpg",
+    };
+  },
+};
+</script>
