7310132a7ec97a97d8f53102e2efbd0b5151f642
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-14 07:07:24 -0700

    update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,9 @@ module.exports = {
       },
       colors: {
         "brand-gray-1": "#dadce0",
-        "brand-blue-1": "#1967d2",
         "brand-green-1": "#137333",
+        "brand-blue-1": "#1967d2",
+        "brand-blue-2": "#4285f4",
       },
       boxShadow: {
         blue: "0 0 3px 3px #4285f4",
