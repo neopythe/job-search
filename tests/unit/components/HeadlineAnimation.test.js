f2bea5ec2c7484acdfbb4f17658d99a8697b4478
Maxwell Rafuse <maxwell.rafuse@proton.me>
2022-12-16 07:12:41 -0700

    refactor HeadlineAnimation tests with beforeEach and afterEach functions

--- tests/unit/components/HeadlineAnimation.test.js
+++ tests/unit/components/HeadlineAnimation.test.js
@@ -4,31 +4,32 @@ import { render, screen } from "@testing-library/vue";
 import HeadlineAnimation from "@/components/HeadlineAnimation.vue";
 
 describe("HeadlineAnimation", () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("displays introductory action verb", () => {
     render(HeadlineAnimation);
     const actionPhrase = screen.getByRole("heading", {
       name: /build for everyone/i,
     });
 
     expect(actionPhrase).toBeInTheDocument();
-
-    vi.useRealTimers();
   });
 
   it("changes action verb at a consistent interval", () => {
-    vi.useFakeTimers();
     const mock = vi.fn();
     vi.stubGlobal("setInterval", mock);
     render(HeadlineAnimation);
 
     expect(mock).toHaveBeenCalled();
-
-    vi.useRealTimers();
   });
 
   it("swaps action verb after interval", async () => {
-    vi.useFakeTimers();
     render(HeadlineAnimation);
     vi.advanceTimersToNextTimer();
     await nextTick();
@@ -37,19 +38,14 @@ describe("HeadlineAnimation", () => {
     });
 
     expect(actionPhrase).toBeInTheDocument();
-
-    vi.useRealTimers();
   });
 
   it("removes interval when component disappears", () => {
-    vi.useFakeTimers();
     const clearInterval = vi.fn();
     vi.stubGlobal("clearInterval", clearInterval);
     const { unmount } = render(HeadlineAnimation);
     unmount();
 
     expect(clearInterval).toHaveBeenCalled();
-
-    vi.useRealTimers();
   });
 });
