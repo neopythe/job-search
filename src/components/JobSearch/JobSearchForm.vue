1dd96eca065ceb61fab34a2979b81f2ce37a7475
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-07 20:49:38 -0700

    update for attributes on JobSearchForm labels

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -9,16 +9,20 @@
     />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
-        <label for="" class="absolute left-0 -top-8">Role</label>
-        <text-input v-model="role" placeholder="Software engineer" />
+        <label for="role" class="absolute left-0 -top-8">Role</label>
+        <text-input id="role" v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3 font-normal"
         >in</span
       >
       <div class="relative flex h-full flex-1 items-center px-3">
-        <label for="" class="absolute left-0 -top-8">Where?</label>
-        <text-input v-model="location" placeholder="Los Angeles" />
+        <label for="location" class="absolute left-0 -top-8">Where?</label>
+        <text-input
+          id="location"
+          v-model="location"
+          placeholder="Los Angeles"
+        />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
