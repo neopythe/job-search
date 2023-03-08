0f90ac41ce1e2fc47c7e201fe869cfaf1dda0aa4
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-08 10:26:11 -0700

    create and render JobListings component

---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>Job Listings</main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <job-filters-sidebar />
+    <job-listings />
   </div>
 </template>
 
 <script>
 import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
 
 export default {
   name: "JobResultsView",
   components: {
     JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
