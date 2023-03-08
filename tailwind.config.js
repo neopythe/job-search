a9610a3db3e19d664b5070dc4df36769a998bdfe
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-08 10:38:43 -0700

    extend Tailwind with new boxShadow option

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,16 @@ module.exports = {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
       colors: {
+        "brand-blue-1": "#1967d2",
+        "brand-blue-2": "#4285f4",
         "brand-gray-1": "#dadce0",
         "brand-gray-2": "#f8f9fa",
         "brand-gray-3": "#80868b",
         "brand-green-1": "#137333",
-        "brand-blue-1": "#1967d2",
-        "brand-blue-2": "#4285f4",
       },
       boxShadow: {
         blue: "0 0 3px 3px #4285f4",
+        gray: "0 1px 3px 0 rgba(60, 64, 67, .3)",
       },
     },
   },
