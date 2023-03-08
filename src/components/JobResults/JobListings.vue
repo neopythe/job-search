dbb4780dcbc3e3250546606418dcfaa0d8f290fd
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-08 10:54:26 -0700

    add job title and location to individual job listing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,5 +1,28 @@
 <template>
-  <main class="flex-auto bg-brand-gray-2 p-8">Job Listings</main>
+  <main class="flex-auto bg-brand-gray-2 p-8">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="borders mx-auto block rounded border-brand-gray-2 bg-white hover:shadow-gray"
+        >
+          <div class="mx-8 border-b border-brand-gray-2 pt-5 pb-2">
+            <h2 class="mb-2 text-2xl font-semibold">
+              Technical Program Manager, Perception, Augmented Reality
+            </h2>
+            <div class="flex align-middle text-sm font-semibold">
+              <div class="mr-5">
+                <span>Gaggle</span>
+              </div>
+              <div>
+                <span>San Francisco, CA, USA</span>
+              </div>
+            </div>
+          </div></router-link
+        >
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
