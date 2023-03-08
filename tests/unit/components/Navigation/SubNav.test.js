762d8f9f6577eb03ba6693add8617a12774f0da9
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-07 18:57:03 -0700

    mock out $route object dependency in SubNav tests

--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -5,16 +5,11 @@ import SubNav from "@/components/Navigation/SubNav.vue";
 describe("SubNav", () => {
   describe("when user is on jobs page", () => {
     it("displays job count", () => {
+      const $route = { name: "JobResults" };
       render(SubNav, {
         global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
+          mocks: { $route },
+          stubs: { FontAwesomeIcon: true },
         },
       });
       const jobCount = screen.getByText("1653");
@@ -25,16 +20,11 @@ describe("SubNav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does not display job count", () => {
+      const $route = { name: "Home" };
       render(SubNav, {
         global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
+          mocks: { $route },
+          stubs: { FontAwesomeIcon: true },
         },
       });
       const jobCount = screen.queryByText("1653");
