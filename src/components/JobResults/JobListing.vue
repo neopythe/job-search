a075de485b7fc5a3e854d41c7e9406deabb0ff65
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-08 11:22:08 -0700

    extract individual job listing to separate JobListing component

---
+++ src/components/JobResults/JobListing.vue
@@ -0,0 +1,50 @@
+<template>
+  <li class="mb-7">
+    <router-link
+      to="/jobs/results/1"
+      class="mx-auto block rounded border border-brand-gray-1 bg-white hover:shadow-gray"
+    >
+      <div class="mx-8 border-b border-brand-gray-1 pt-5 pb-2">
+        <h2 class="mb-2 text-2xl font-semibold">
+          Technical Program Manager, Perception, Augmented Reality
+        </h2>
+        <div class="flex py-1 align-middle text-sm font-semibold">
+          <div class="mr-5">
+            <span>Gaggle</span>
+          </div>
+          <div>
+            <span>San Francisco, CA, USA</span>
+          </div>
+        </div>
+      </div>
+      <div class="px-8 py-4">
+        <div>
+          <h3 class="mt-1 mb-2 font-semibold">Qualifications:</h3>
+          <div>
+            <ul
+              class="flex list-disc flex-col gap-1 pl-8 marker:text-brand-gray-1"
+            >
+              <li>Bachelor's degree or equivalent practical experience</li>
+              <li>5 years of experience in program management</li>
+              <li>
+                Experience analyzing data through querying database (e.g. SQL),
+                using spreadsheet software, and creating statistical models
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="mt-4 text-center text-sm font-semibold">
+          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+            >Expand</router-link
+          >
+        </div>
+      </div></router-link
+    >
+  </li>
+</template>
+
+<script>
+export default {
+  name: "JobListing",
+};
+</script>
