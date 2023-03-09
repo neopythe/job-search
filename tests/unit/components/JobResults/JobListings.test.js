9ac7df5d467c2f758dcd733f116be4392b0585ea
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-09 15:20:15 -0700

    fix failing JobListings tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -9,15 +9,26 @@ vi.mock("axios");
 describe("JobListings", () => {
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
+    const $route = { query: { page: 5 } };
+    render(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
 
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("displays maximum of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = { query: { page: 1 } };
     render(JobListings, {
       global: {
+        mocks: {
+          $route,
+        },
         stubs: {
           "router-link": RouterLinkStub,
         },
