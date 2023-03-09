69bc7c4032b92e78b14312dc1bbf19f94483057f
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-09 08:02:59 -0700

    set a limit of 10 displayed jobs for JobListings

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -14,7 +14,7 @@ describe("JobListings", () => {
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("creates a job listing for every job", async () => {
+  it("displays maximum of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     render(JobListings, {
       global: {
@@ -25,6 +25,6 @@ describe("JobListings", () => {
     });
     const jobListings = await screen.findAllByRole("listitem");
 
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
