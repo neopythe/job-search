4f4e67eb26c73ad2c07dcb0ab85047a6101ea1c1
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-11 13:50:27 -0700

    display page number at bottom of JobListings component

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -3,6 +3,11 @@
     <ol>
       <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="mx-auto mt-10">
+      <div class="flex flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -22,8 +27,11 @@ export default {
     };
   },
   computed: {
+    currentPage() {
+      return this.$route.query?.page || 1;
+    },
     displayedJobs() {
-      const page = this.$route.query?.page || 1;
+      const page = this.currentPage;
       return this.jobs.slice(page * 10 - 10, page * 10);
     },
   },
