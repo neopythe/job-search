c1f9b66f738a0bb358e7ba13c6632d9908ab0840
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-03-12 08:52:37 -0600

    register Pinia store

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +11,10 @@ import "@/index.css";
 
 library.add(faSearch);
 
+const pinia = createPinia();
+
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
