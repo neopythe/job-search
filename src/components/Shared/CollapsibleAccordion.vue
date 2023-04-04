4b4f3503fac462e9f08f81cc216af9332ff8d2b9
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-04 13:55:37 -0600

    wire up and style CollapsibleAccordion component

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>My accordion</div>
+  <div class="border-b border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="toggleIsOpen"
+    >
+      <h3 class="font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+    <div v-if="isOpen" class="mt-5 w-full">Child</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CollapsibleAccordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    toggleIsOpen() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
