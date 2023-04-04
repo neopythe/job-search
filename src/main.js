c1c59ae176c7dfd478fa9a9cbbf9338c49fa2219
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-04 12:10:52 -0600

    import additional Font Awesome icons

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import App from "@/App.vue";
 
 import router from "@/router";
 import "@/index.css";
 
+library.add(faAngleDown);
+library.add(faAngleUp);
 library.add(faSearch);
 
 const pinia = createPinia();
