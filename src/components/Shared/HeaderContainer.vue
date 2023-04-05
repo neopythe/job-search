d691aac45be32f5f906102fe3470b937987bbe89
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-05 15:25:47 -0600

    increase vertical margin

--- src/components/Shared/HeaderContainer.vue
+++ src/components/Shared/HeaderContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full border-b border-brand-gray-1">
-    <div class="mx-auto my-16 text-center">
+    <div class="mx-auto my-20 text-center">
       <slot name="title">Oops</slot>
       <slot name="subtitle">Looks like someone forgot to populate me!</slot>
     </div>
