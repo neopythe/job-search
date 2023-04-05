f61aacce22110697a4251838da74f95ef783256d
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-05 13:35:27 -0600

    create HeaderContainer component

---
+++ src/components/Shared/HeaderContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full border-b border-brand-gray-1">
+    <div class="mx-auto my-16 text-center">
+      <slot name="title">Oops</slot>
+      <slot name="subtitle">Looks like someone forgot to populate me!</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderContainer",
+};
+</script>
