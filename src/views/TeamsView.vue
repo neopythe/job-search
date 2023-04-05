50150da462965fbbeca7711b309205a469eaa477
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-05 15:26:29 -0600

    update markup with slot content

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,17 @@
 <template>
-  <header-container></header-container>
+  <header-container>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl">Teams</h1>
+    </template>
+    <template v-slot:subtitle>
+      <div class="mx-auto max-w-screen-sm">
+        <h2 class="my-4 w-full px-4 text-sm font-light">
+          Together, we create access to information and build products that
+          improve people's lives. Want to be a Gaggler? Find your role.
+        </h2>
+      </div>
+    </template>
+  </header-container>
 </template>
 
 <script>
