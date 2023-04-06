9053344abd01968590154f38f0eb9001906fb859
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-06 02:34:18 -0600

    render SpotlightGallery component

--- src/components/JobSearch/HeroSection.vue
+++ src/components/JobSearch/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex h-screen flex-col">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
         <div class="col-span-5 col-start-2">
@@ -17,18 +17,34 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+    <spotlight-gallery class="flex justify-center pb-16">
+      <template #default="{ spotlight }">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex h-72 w-64 flex-col overflow-hidden rounded-lg border border-brand-gray-1 shadow-brand-gray-3 transition ease-in-out hover:shadow-xl"
+        >
+          <img :src="spotlight.img" alt="" class="h-2/5 object-cover" />
+          <div class="h-48 p-3">
+            <h3 class="text-lg font-semibold">{{ spotlight.title }}</h3>
+            <p class="mt-3 text-sm">{{ spotlight.description }}</p>
+          </div>
+        </router-link>
+      </template>
+    </spotlight-gallery>
   </main>
 </template>
 
 <script>
 import HeadlineAnimation from "@/components/JobSearch/HeadlineAnimation.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+import SpotlightGallery from "@/components/JobSearch/SpotlightGallery.vue";
 
 export default {
   name: "HeroSection",
   components: {
     HeadlineAnimation,
     JobSearchForm,
+    SpotlightGallery,
   },
 };
 </script>
