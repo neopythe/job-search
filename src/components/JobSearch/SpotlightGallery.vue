0ab336d09f754949644152daaa5f9a39fb3b1805
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-06 01:20:57 -0600

    create SpotlightGallery component

---
+++ src/components/JobSearch/SpotlightGallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SpotlightGallery",
+  data() {
+    return { spotlights: [] };
+  },
+  async mounted() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const { data } = await axios.get(`${baseUrl}/spotlights`);
+    this.spotlights = data;
+  },
+};
+</script>
