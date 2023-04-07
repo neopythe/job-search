8756b675232e43719c8ff442d26d9d8b879278b0
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-07 00:07:26 -0600

    add test for checkbox interaction

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 
 import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
 
 import JobFiltersSidebarOrganizations from "@/components/JobResults/JobfiltersSidebar/JobFiltersSidebarOrganizations.vue";
 
@@ -20,5 +21,25 @@ describe("JobFiltersSidebarOrganizations", () => {
     const organizations = organizationListItems.map((node) => node.textContent);
 
     expect(organizations).toEqual(["Google", "Amazon"]);
+  });
+
+  it.only("communicates that user has selected checkbox for organization", async () => {
+    const pinia = createTestingPinia();
+    const jobsStore = useJobsStore();
+    const userStore = useUserStore();
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon", "Facebook"]);
+    render(JobFiltersSidebarOrganizations, {
+      global: { plugins: [pinia], stubs: { FontAwesomeIcon: true } },
+    });
+    const button = screen.getByRole("button", { name: /organizations/i });
+    await userEvent.click(button);
+    const googleCheckbox = screen.getByRole("checkbox", {
+      name: /google/i,
+    });
+    await userEvent.click(googleCheckbox);
+
+    expect(userStore.ADD_SELECTED_ORGANIZATIONS).toHaveBeenCalledWith([
+      "Google",
+    ]);
   });
 });
