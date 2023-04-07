84c9863a6ecb638c67551db1383ceae7fc15786b
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-06 18:11:29 -0600

    add v-model to checkbox for organizations

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -8,7 +8,13 @@
             :key="organization"
             class="flex h-8 w-1/2 items-center"
           >
-            <input id="organization" type="checkbox" class="mr-3" />
+            <input
+              id="organization"
+              v-model="selectedOrganizations"
+              :value="organization"
+              type="checkbox"
+              class="mr-3"
+            />
             <label for="organization">{{ organization }}</label>
           </li>
         </ul>
@@ -27,6 +33,9 @@ import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 export default {
   name: "JobFiltersSidebarOrganizations",
   components: { CollapsibleAccordion },
+  data() {
+    return { selectedOrganizations: [] };
+  },
   computed: {
     ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS]),
   },
