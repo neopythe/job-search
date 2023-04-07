88eb3a20ef74342cb1aa8578519ede72ee66107c
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-06 18:06:53 -0600

    test UNIQUE_ORGANIZATIONS getter with component

---
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -0,0 +1,24 @@
+import { createTestingPinia } from "@pinia/testing";
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
+import { useJobsStore } from "@/stores/jobs";
+
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobfiltersSidebar/JobFiltersSidebarOrganizations.vue";
+
+describe("JobFiltersSidebarOrganizations", () => {
+  it("renders list of unique organizations from jobs", async () => {
+    const pinia = createTestingPinia();
+    const jobsStore = useJobsStore();
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+    render(JobFiltersSidebarOrganizations, {
+      global: { plugins: [pinia], stubs: { FontAwesomeIcon: true } },
+    });
+    const button = screen.getByRole("button", { name: /organizations/i });
+    await userEvent.click(button);
+    const organizationListItems = screen.getAllByRole("listitem");
+    const organizations = organizationListItems.map((node) => node.textContent);
+
+    expect(organizations).toEqual(["Google", "Amazon"]);
+  });
+});
