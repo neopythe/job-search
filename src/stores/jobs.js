068e099a59842b5df14b2500a2b81e14bbf29980
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-06 22:20:49 -0600

    update getJobs import with absolute path

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 
-import getJobs from "../api/getJobs";
+import getJobs from "@/api/getJobs";
 
 export const FETCH_JOBS = "FETCH_JOBS";
 export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
