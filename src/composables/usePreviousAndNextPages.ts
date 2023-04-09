345c64c6fe52bd3a2676936075921f712aecf8af
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-09 12:31:18 -0600

    add types to usePreviousAndNextPages composable and update corresponding tests

--- src/composables/usePreviousAndNextPages.ts
+++ src/composables/usePreviousAndNextPages.ts
@@ -1,6 +1,9 @@
-import { computed } from "vue";
+import { type Ref, computed } from "vue";
 
-const usePreviousAndNextPages = (currentPage, maxPage) => {
+const usePreviousAndNextPages = (
+  currentPage: Ref<number>,
+  maxPage: Ref<number>
+) => {
   const previousPage = computed(() => {
     const previousPage = currentPage.value - 1;
     return previousPage >= 1 ? previousPage : null;
