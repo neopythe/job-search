6556c981fd507e4123ef3a96079352336f042abd
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-09 10:49:19 -0600

    define types for user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -4,17 +4,23 @@ export const ADD_SELECTED_JOB_TYPES = "ADD_SELECTED_JOB_TYPES";
 export const ADD_SELECTED_ORGANIZATIONS = "ADD_SELECTED_ORGANIZATIONS";
 export const LOGIN_USER = "LOGIN_USER";
 
+export interface UserState {
+  isLoggedIn: boolean;
+  selectedOrganizations: string[];
+  selectedJobTypes: string[];
+}
+
 export const useUserStore = defineStore("user", {
-  state: () => ({
+  state: (): UserState => ({
     isLoggedIn: false,
     selectedJobTypes: [],
     selectedOrganizations: [],
   }),
   actions: {
-    [ADD_SELECTED_JOB_TYPES](jobTypes) {
+    [ADD_SELECTED_JOB_TYPES](jobTypes: string[]) {
       this.selectedJobTypes = jobTypes;
     },
-    [ADD_SELECTED_ORGANIZATIONS](organizations) {
+    [ADD_SELECTED_ORGANIZATIONS](organizations: string[]) {
       this.selectedOrganizations = organizations;
     },
     [LOGIN_USER]() {
