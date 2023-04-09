856ec23f7dee2aea91314e59687a2ad799cc39bd
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-09 10:29:25 -0600

    update getJobs API call with TypeScript

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+import type { Job } from "@/api/types";
+
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL;
-  const { data } = await axios.get(`${baseUrl}/jobs`);
+  const { data } = await axios.get<Job[]>(`${baseUrl}/jobs`);
   return data;
 };
 
