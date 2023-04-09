1b293042886af1cfac8fdeabd8915e935d642c55
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-09 10:56:55 -0600

    define types for jobs store

--- src/stores/jobs.ts
+++ src/stores/jobs.ts
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 
+import type { Job } from "@/api/types";
+
 import getJobs from "@/api/getJobs";
 
 import { useUserStore } from "@/stores/user";
@@ -11,8 +13,12 @@ export const INCLUDE_JOB_BY_ORGANIZATION = "INCLUDE_JOB_BY_ORGANIZATION";
 export const UNIQUE_JOB_TYPES = "UNIQUE_JOB_TYPES";
 export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
 
+export interface JobsState {
+  jobs: Job[];
+}
+
 export const useJobsStore = defineStore("jobs", {
-  state: () => ({
+  state: (): JobsState => ({
     jobs: [],
   }),
   actions: {
@@ -22,14 +28,14 @@ export const useJobsStore = defineStore("jobs", {
     },
   },
   getters: {
-    [FILTERED_JOBS]({ jobs }) {
+    [FILTERED_JOBS]({ jobs }): Job[] {
       return jobs
         .filter((job) => this.INCLUDE_JOB_BY_JOB_TYPE(job))
         .filter((job) => this.INCLUDE_JOB_BY_ORGANIZATION(job));
     },
     [INCLUDE_JOB_BY_JOB_TYPE]:
       () =>
-      ({ jobType }) => {
+      ({ jobType }: Job) => {
         const { selectedJobTypes } = useUserStore();
         return selectedJobTypes.length > 0
           ? selectedJobTypes.includes(jobType)
@@ -37,19 +43,19 @@ export const useJobsStore = defineStore("jobs", {
       },
     [INCLUDE_JOB_BY_ORGANIZATION]:
       () =>
-      ({ organization }) => {
+      ({ organization }: Job) => {
         const { selectedOrganizations } = useUserStore();
         return selectedOrganizations.length > 0
           ? selectedOrganizations.includes(organization)
           : true;
       },
     [UNIQUE_JOB_TYPES]({ jobs }) {
-      const uniqueJobTypes = new Set();
+      const uniqueJobTypes = new Set<string>();
       jobs.forEach(({ jobType }) => uniqueJobTypes.add(jobType));
       return uniqueJobTypes;
     },
     [UNIQUE_ORGANIZATIONS]({ jobs }) {
-      const uniqueOrganizations = new Set();
+      const uniqueOrganizations = new Set<string>();
       jobs.forEach(({ organization }) => uniqueOrganizations.add(organization));
       return uniqueOrganizations;
     },
