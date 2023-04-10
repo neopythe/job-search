a05afe40266bac44f4cffe94cedd48c81410ddff
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-10 13:44:47 -0600

    wire up click handler for clearing filters

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -15,7 +15,11 @@
       <div class="flex flex-row justify-between">
         <h3 class="my-3 font-semibold">What do you want to do?</h3>
         <div class="flex items-center text-sm">
-          <action-button text="Clear filters" type="secondary" />
+          <action-button
+            text="Clear filters"
+            type="secondary"
+            @click="userStore.CLEAR_FILTERS"
+          />
         </div>
       </div>
       <job-filters-sidebar-checkbox-group
