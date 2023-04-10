48e50edc9e136513019211665fa5c1941c7d4e85
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-10 10:35:09 -0600

    create FETCH_DEGREES action via TDD

--- src/stores/degrees.ts
+++ src/stores/degrees.ts
@@ -3,8 +3,15 @@ import { defineStore } from "pinia";
 
 import type { Degree } from "@/api/types";
 
+import getDegrees from "@/api/getDegrees";
+
 export const useDegreesStore = defineStore("degrees", () => {
   const degrees = ref<Degree[]>([]);
 
-  return { degrees };
+  const FETCH_DEGREES = async () => {
+    const receivedDegrees = await getDegrees();
+    degrees.value = receivedDegrees;
+  };
+
+  return { degrees, FETCH_DEGREES };
 });
--- tests/unit/stores/degrees.test.ts
+++ tests/unit/stores/degrees.test.ts
@@ -1,6 +1,13 @@
+import type { Mock } from "vitest";
+
 import { createPinia, setActivePinia } from "pinia";
+import axios from "axios";
 
 import { useDegreesStore } from "@/stores/degrees";
+
+vi.mock("axios");
+
+const axiosGetMock = axios.get as Mock;
 
 describe("state", () => {
   beforeEach(() => {
@@ -11,5 +18,23 @@ describe("state", () => {
     const store = useDegreesStore();
 
     expect(store.degrees).toEqual([]);
+  });
+});
+
+describe("actions", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("FETCH_DEGREES", () => {
+    it("makes API request and stores received degrees", async () => {
+      axiosGetMock.mockResolvedValue({
+        data: [{ id: 1, degree: "Bachelor's" }],
+      });
+      const store = useDegreesStore();
+      await store.FETCH_DEGREES();
+
+      expect(store.degrees).toEqual([{ id: 1, degree: "Bachelor's" }]);
+    });
   });
 });
