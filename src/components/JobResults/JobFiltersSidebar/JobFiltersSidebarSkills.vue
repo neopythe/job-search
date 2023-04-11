7c6a0fe15f863b91581811d6e26bf120ce8a9844
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-11 05:34:00 -0600

    wire up search state to Pinia store

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="mt-2 mb-4">
     <input
+      v-model="skillsSearchTerm"
       type="text"
       class="h-12 w-full rounded border border-brand-gray-1 p-3 shadow-gray"
       placeholder="Computer programming, finance degree"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+
+const skillsSearchTerm = computed({
+  get: () => userStore.skillsSearchTerm,
+  set: (value: string) => userStore.UPDATE_SKILLS_SEARCH_TERM(value),
+});
+</script>
